2e14efbfe1a34819aaae14856a38a26fab321cf2
Pfulg, Daniel <daniel.pfulg@zuehlke.com>
2019-12-03 07:02:42 +0100

    OWIN

---
+++ DotNetBay.Health.Owin/HealthFeature.cs
@@ -0,0 +1,18 @@
+﻿using Owin;
+
+namespace DotNetBay.Health.Owin
+{
+    public static class HealthFeature
+    {
+        public static void UseHealth(this IAppBuilder appBuilder, string path)
+        {
+            appBuilder.Map(path, ab => ab.Use<HealthMiddleware>());
+        }
+
+        public static void UseHealth(this IAppBuilder appBuilder)
+        {
+            appBuilder.Use<HealthMiddleware>();
+        }
+
+    }
+}
---
+++ DotNetBay.SelfHost/Startup.cs
@@ -0,0 +1,17 @@
+﻿using DotNetBay.Health.Owin;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DotNetBay.SelfHost.Startup))]
+
+namespace DotNetBay.SelfHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHealth("/health");
+            //app.UseHealth();
+        }
+    }
+}
